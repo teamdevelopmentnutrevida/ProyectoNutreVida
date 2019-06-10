0348fd8074b8299a0af64345bdcbd8a76a28ea94
juandi perez <juandi199847@hotmail.com>
2019-06-09 19:47:37 -0600

    Un poco de bl

---
+++ NutreVida/BL/HistorialMedico.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class HistorialMedico
+    {
+        public int Cedula { set; get; }
+        public string Antecedentes { set; get; }
+        public string Patologias { set; get; }
+        public int ConsumeLicor { set; get; }
+        public int Fuma { set; get; }
+        public string FrecFuma { set; get; }
+        public string FrecLicor { set; get; }
+        public DateTime UltimoExamen { set; get; }
+        public string ActividadFisica;
+
+        public HistorialMedico() { }
+
+        public HistorialMedico(int cedula, string antecedentes, string patologias, int consumeLicor, int fuma, string frecFuma, string frecLicor, DateTime ultimoExamen, string actividadFisica)
+        {
+            Cedula = cedula;
+            Antecedentes = antecedentes;
+            Patologias = patologias;
+            ConsumeLicor = consumeLicor;
+            Fuma = fuma;
+            FrecFuma = frecFuma;
+            FrecLicor = frecLicor;
+            UltimoExamen = ultimoExamen;
+            ActividadFisica = actividadFisica;
+        }
+    }
+}
