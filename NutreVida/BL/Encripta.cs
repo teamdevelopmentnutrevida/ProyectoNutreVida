4c9e61f0bb72ab285787b5e4ff1d556519e80a8c
tannia elizondo cespedes <tannia130@hotmail.com>
2019-06-24 01:42:09 -0600

    Clase encripcion de contraseña

---
+++ NutreVida/BL/Encripta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Encripta
+    {
+
+        public string EncodePassword(string originalPassword)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(originalPassword);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+
+
+        public Boolean desEncode(string contraEncriptada, string contra)
+        {
+            Boolean coincide = false;
+            string result = string.Empty;
+
+
+            byte[] decryted = Convert.FromBase64String(contraEncriptada);
+            result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+            result = System.Text.Encoding.Unicode.GetString(decryted);
+            if (result.Equals(contra))
+            {
+                coincide = true;
+            }
+            else {
+                coincide = false;
+            }
+            return coincide;
+        } 
+
+    }
+}
